7b05b7a79175421d18f808678f14181466150f65
gmbabreu <gmbabreu@gmail.com>
2026-07-14 17:04:24 -0400

    update spring calibration

--- Data_Generation/calibrateSpring.m
+++ Data_Generation/calibrateSpring.m
@@ -6,29 +6,35 @@ close all
 
 D = 48.5;      % Distance from camera to mirror (in)
 xC = 18;       % Closest ruler position to the camera (in)
+g = 980;       % Acceleration due to gravity (cm/s^2)
+a = 5.95/2;      % Distance from mirror to spring (cm)
 
-hook_weight = 0.148;      % kg
-added_weight = 0.182;     % kg
+hook_mass = 0.148;      % g
+added_mass = 0.182;     % g
 
 % Total applied weights
-weights = hook_weight + (0:8)*added_weight;
+mass = hook_mass + (0:8)*added_mass;
+
+% Torque
+torque = mass * g * 2 * a;
 
 %% Measured ruler positions (inches)
 
 forward = [5, 8.375, 11.375, 15, 18, 21.875, 24.75, 28.125, 32];
 backward = [6.625, 9.75, 11.625, 15.375, 18.25, 21.75, 24.75, 28.375, 31.875];
+% Join both
 
 %% Convert ruler position to mirror angle (radians)
 
-thetaForward = atan((forward - xC)/D);
-thetaBackward = atan((backward - xC)/D);
+thetaForward = atan((forward - xC)/D)/2;
+thetaBackward = atan((backward - xC)/D)/2;
 thetaMean = (thetaForward + thetaBackward)/2;
 
 %% Linear fits
 
-forwardFit = polyfit(thetaForward, weights, 1);
-backwardFit = polyfit(thetaBackward, weights, 1);
-meanFit = polyfit(thetaMean, weights, 1);
+forwardFit = polyfit(thetaForward, torque, 1);
+backwardFit = polyfit(thetaBackward, torque, 1);
+meanFit = polyfit(thetaMean, torque, 1);
 
 %% Evaluate fitted lines
 
@@ -46,13 +52,13 @@ meanLine = polyval(meanFit, thetaPlot);
 figure
 hold on
 
-plot(thetaForward, weights, 'bo', 'MarkerFaceColor','b')
-plot(thetaBackward, weights, 'ro', 'MarkerFaceColor','r')
+plot(thetaForward, torque, 'bo', 'MarkerFaceColor','b')
+plot(thetaBackward, torque, 'ro', 'MarkerFaceColor','r')
 
 plot(thetaPlot, meanLine, 'k-', 'LineWidth',2)
 
 xlabel('\theta (radians)')
-ylabel('Weight (kg)')
+ylabel('Torque (g*cm)')
 legend('Forward','Backward', ...
        'Mean fit', ...
        'Location','best')
@@ -73,7 +79,7 @@ fprintf("\nMean:\n");
 fprintf("Weight = %.6f * theta + %.6f\n", ...
     meanFit(1), meanFit(2));
 
-fprintf("\nSpring calibration coefficients (kg/rad):\n");
+fprintf("\nSpring calibration coefficients (dyn*cm/rad):\n");
 fprintf("Forward : %.6f\n", forwardFit(1));
 fprintf("Backward: %.6f\n", backwardFit(1));
 fprintf("Mean    : %.6f\n", meanFit(1));
